d1a9cee6122cabb89604f97bd50472ef1887c2d1
Truc <icloud07072010@gmail.com>
2026-09-13 14:18:41 +0700

    .

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCR2lbKd2TEPl-01VObuV7eWfcSkpacfZA",
-  authDomain: "englishproject-c0131.firebaseapp.com",
-  projectId: "englishproject-c0131",
-  storageBucket: "englishproject-c0131.firebasestorage.app",
-  messagingSenderId: "270223735960",
-  appId: "1:270223735960:web:6d4aa35c04993b0f7724ed",
-  measurementId: "G-GD5P6V42DW"
+  apiKey: "AIzaSyAbFxplx12l0QriZlyPwd_ipH3irMbaDOo",
+  authDomain: "indivitalenglishbook.firebaseapp.com",
+  projectId: "indivitalenglishbook",
+  storageBucket: "indivitalenglishbook.firebasestorage.app",
+  messagingSenderId: "75132327202",
+  appId: "1:75132327202:web:ffbad14bad2f46cac1817b",
+  measurementId: "G-NXC0WJ4YN1"
 };
+
